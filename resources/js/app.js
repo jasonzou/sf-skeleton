610b041f5ea906fa04b960d458de4a8d5fa047f4
jasonzou <jason.zou@gmail.com>
2018-12-11 19:15:55 -0500

    vue.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
   
-import Example from './components/Example'
+import example from './components/Example'
   
 /**
  * Create a fresh Vue Application instance
  */
 new Vue({
    el: '#app',
-   components: {Example}
+   components: {example}
 });
-  
+
+require('../sass/app.scss');
